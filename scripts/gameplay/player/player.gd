c14e2a3c6162307ad9a3fd75da4b310e5d45db6b
Gede Hari <harikresnagede@gmail.com>
2020-11-23 18:49:17 +0800

    Added the ability to stun players with dash, added trail effect

--- scripts/gameplay/player/player.gd
+++ scripts/gameplay/player/player.gd
@@ -3,11 +3,15 @@ extends RigidBody2D
 export var speed : float = 600
 
 onready var sprite = $Sprite
-onready var look_at_node = $LookAt
+onready var trail = $Trail
 onready var arrow = $Arrow
+
+onready var look_at_node = $LookAt
 onready var player_name = $PlayerName
-onready var dash_timer = $DashTimer              # Dash timer
+
+onready var dash_timer = $DashTimer              # Dash timer.
 onready var dash_cd_timer = $DashCooldownTimer   # Dash cooldown timer.
+onready var stun_timer = $StunTimer              # Stun timer.
 
 onready var debug_label = $DebugLabel
 
@@ -21,6 +25,7 @@ var la_rot : float
 var vel : Vector2
 
 var is_dashing : bool = false
+var is_stunned : bool = false
 
 
 func _ready() -> void:
@@ -32,6 +37,7 @@ func _ready() -> void:
 		# If this is the current scene, enable test mode.
 		test_mode = true
 	
+	trail.emitting = false
 	arrow.visible = is_master or test_mode
 	debug_label.visible = is_master or test_mode
 
@@ -46,24 +52,33 @@ func _physics_process(delta: float) -> void:
 		# Update arrow rotation, but with lerp
 		arrow.rotation = lerp_angle(arrow.rotation, la_rot, 0.175)
 		# Do movement
-		move()
+		if is_stunned:
+			if stun_timer.time_left == 0:
+				is_stunned = false
+				modulate.v = 1
+		else:
+			move()
 		
 		if Network.enet:
 			# Send state every physics process. I don't know if this is a good idea or not.
-			rpc_unreliable("_set_state", position, linear_velocity, applied_force)
+			rpc_unreliable("_set_state", position, linear_velocity, applied_force, modulate, trail.emitting)
 
-
-puppet func _set_state(pos : Vector2, vel : Vector2, frc : Vector2) -> void:
+# Set state of player. Sent to puppet nodes.
+puppet func _set_state(pos : Vector2, vel : Vector2, force : Vector2, mod : Color, t_emit : bool) -> void:
 	position = pos
 	linear_velocity = vel
-	applied_force = frc
+	applied_force = force
+	modulate = mod
+	trail.emitting = t_emit
 
 
+# Normal move
 func move() -> void:
 	vel = Vector2(cos(la_rot), sin(la_rot))
 	
 	if Input.is_action_just_pressed("dash") and dash_cd_timer.time_left == 0 and not is_dashing:
 		is_dashing = true
+		trail.emitting = true
 		dash_timer.start()
 	
 	if is_dashing:
@@ -72,12 +87,32 @@ func move() -> void:
 		
 		if dash_timer.is_stopped():
 			is_dashing = false
+			trail.emitting = false
 			dash_cd_timer.start()
 	else:
 		la_rot = look_at_node.rotation
 		applied_force = vel * speed
 
 
+# Stun this player, likely be called from another player.
+remote func stun(id : int = 0) -> void:
+	if not is_network_master():
+		rpc_id(id, "stun")
+	else:
+		is_stunned = true
+		applied_force = Vector2.ZERO
+		modulate.v = 0.5
+		stun_timer.start()
+
+
 func _on_Player_body_entered(body: Node) -> void:
 	# Stop dash if player hits anything.
-	dash_timer.stop()
+	if is_network_master():
+		if is_dashing:
+			if body.is_in_group("player"):
+				body.stun(body.get_network_master())
+			dash_timer.stop()
+
+
+func _on_PlayerDetectorBody_body_entered(body: Node) -> void:
+	pass
